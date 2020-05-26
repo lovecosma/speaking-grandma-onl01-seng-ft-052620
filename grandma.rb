df2b9b1c9f7f64ccefc7e1d3826e663106831ed0
candescent-torrent-9701 <williamsmatthew333@gmail.com>
2020-05-26 17:04:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,7 @@ if @phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
 elsif @phrase == @phrase.upcase
   return "NO, NOT SINCE 1938!"
-else @phrase != @phrase.uppercase
+else @phrase != @phrase.upcase
   return "HUH?! SPEAK UP, SONNY!"
 end
 end
